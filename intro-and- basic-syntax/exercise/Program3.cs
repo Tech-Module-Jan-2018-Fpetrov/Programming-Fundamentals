6b262ca7cb83719e9f65ff68befd91c78e37269d
Ani <aneliqsavovich@gmail.com>
2018-03-19 12:29:39 +0200

    solution for Problem 3

---
+++ intro-and- basic-syntax/exercise/Program3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            double miles = double.Parse(Console.ReadLine());
+            var kilometers = miles * 1.60934;
+
+            Console.WriteLine("{0:F2}", kilometers);
+
+        }
+    }
+}
+    
