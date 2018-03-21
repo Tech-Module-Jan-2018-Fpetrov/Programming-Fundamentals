ab4d4c8356859d80d5a32b59d194e02519a729a0
Snezhana Paykova <paykova@abv.bg>
2018-03-21 12:16:31 +0200

    Upload the solution of problem2 - Rectangle Area.

---
+++ intro-and- basic-syntax/exercise/problem2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02.Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = float.Parse(Console.ReadLine());
+            var height = float.Parse(Console.ReadLine());
+            var sum = width * height;
+
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
