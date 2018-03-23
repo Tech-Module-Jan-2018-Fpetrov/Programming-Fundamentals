e492514b051ff93b20aa83f7ba09b0580074a3aa
Filip <fpetrov8906@gmail.com>
2018-03-23 13:14:42 +0200

    Added description of operations in problem2.cs.

--- intro-and- basic-syntax/exercise/problem2.cs
+++ intro-and- basic-syntax/exercise/problem2.cs
@@ -6,10 +6,15 @@ namespace _02.Rectangle_Area
     {
         static void Main(string[] args)
         {
+			// We have to read WIDTH from Console.
             var width = float.Parse(Console.ReadLine());
+			// We have to read HEIGHT from Console.
             var height = float.Parse(Console.ReadLine());
+			
+			// We create new VAR with name SUM. Now we have to multiply WIDTH and HEIGHT.
             var sum = width * height;
-
+			
+			// Print the result of SUM.
             Console.WriteLine($"{sum:F2}");
         }
     }
