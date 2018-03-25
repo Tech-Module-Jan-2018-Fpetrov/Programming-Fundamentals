5399433734c136afbd8e50fa37aa0c0d2f35b1eb
Filip <fpetrov8906@gmail.com>
2018-03-25 11:42:08 +0300

    Change the variable name of problem2.cs

--- intro-and- basic-syntax/exercise/problem2.cs
+++ intro-and- basic-syntax/exercise/problem2.cs
@@ -7,12 +7,12 @@ namespace _02.Rectangle_Area
         static void Main(string[] args)
         {
 			// We have to read WIDTH from Console.
-            var width = float.Parse(Console.ReadLine());
+            float width = float.Parse(Console.ReadLine());
 			// We have to read HEIGHT from Console.
-            var height = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
 			
 			// We create new VAR with name SUM. Now we have to multiply WIDTH and HEIGHT.
-            var sum = width * height;
+            float sum = width * height;
 			
 			// Print the result of SUM.
             Console.WriteLine($"{sum:F2}");
